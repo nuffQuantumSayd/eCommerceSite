b8cb2283545a1215c27bce72af2ebdedb285bb19
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-08-01 15:13:44 -0700

    Made a create game view

---
+++ eCommerceSite/Controllers/GamesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSite.Controllers
+{
+    public class GamesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}

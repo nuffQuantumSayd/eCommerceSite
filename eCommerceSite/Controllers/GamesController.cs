16544fe94dcc779570aec8dd79d41f342bf73e69
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-08-08 13:37:15 -0700

    Game Update is functional

--- eCommerceSite/Controllers/GamesController.cs
+++ eCommerceSite/Controllers/GamesController.cs
@@ -54,5 +54,19 @@ namespace eCommerceSite.Controllers
             }
             return View(gameToEdit);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Game gameModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Games.Update(gameModel);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(gameModel);
+        }
     }
 }

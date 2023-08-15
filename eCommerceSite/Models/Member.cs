ede22e47925a1731f39bff11bf9cfd932d8dff80
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-08-15 13:16:03 -0700

    Added register view model

--- eCommerceSite/Models/Member.cs
+++ eCommerceSite/Models/Member.cs
@@ -15,4 +15,26 @@ namespace eCommerceSite.Models
 
         public int Username { get; set; }
     }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Email")]
+        [Compare(nameof(Email))]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [StringLength(75, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
 }

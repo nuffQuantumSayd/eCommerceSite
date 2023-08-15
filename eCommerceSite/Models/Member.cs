ebe924613a8ee25f3c677c4077c7aaa342577fbc
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-08-15 12:57:58 -0700

    Add member class

---
+++ eCommerceSite/Models/Member.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceSite.Models
+{
+    public class Member
+    {
+        [Key]
+        public int MemberId { get; set; }
+
+        public int Email { get; set; }
+
+        public int Password { get; set; }
+
+        public int Phone { get; set; }
+
+        public int Username { get; set; }
+    }
+}

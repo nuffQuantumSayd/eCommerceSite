e81119f6565c2d054243e9506193bd3e39833b66
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-08-16 12:08:41 -0700

    Added HttpPost for register

--- eCommerceSite/Controllers/MembersController.cs
+++ eCommerceSite/Controllers/MembersController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace eCommerceSite.Controllers
 {
     public class MembersController : Controller
     {
+        private readonly VideoGameContext _context;
+        public MembersController(VideoGameContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel regModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                //Map regModel data to member object
+                Member newMember = new()
+                {
+                    Email = regModel.Email,
+                    Password = regModel.Password,
+                };
+                
+                _context.Members.Add(newMember);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(regModel);
         }
     }
 }

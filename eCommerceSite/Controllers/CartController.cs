4f513248c09224e904f1b738e54b327ff9de564c
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-08-30 15:32:21 -0700

    Add shopping cart button to game catalog

---
+++ eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,30 @@
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly VideoGameContext _context;
+
+        public CartController(VideoGameContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            Game? gameToAdd = _context.Games.Where(g => g.GameId == id)
+                                           .SingleOrDefault();
+            if (gameToAdd == null)
+            {
+                TempData["Message"] = "Sorry that game no longer exists";
+                return RedirectToAction("Index", "Games");
+            }
+
+            TempData["Message"] = "Item added to cart";
+            return RedirectToAction("Index", "Games");
+        }
+    }
+}
